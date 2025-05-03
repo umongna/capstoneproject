0319f2491a0a22eac9d4418c910c8816029e3d22
umongna <165342875+umongna@users.noreply.github.com>
2025-05-03 11:03:16 -0400

    Update settings.py

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -5,7 +5,7 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+#
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ')_z--t-qq1=s!l*c-1pg(%$3l%=ys9m7!fh@jtom47ozn-24^*'
